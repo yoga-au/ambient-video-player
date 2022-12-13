f9aba42ece263668fbc3680ebe45450c969225af
Yoga Agung Utama <yogaagung.utama@gmail.com>
2022-12-13 09:58:13 +0700

    add more example, heading text, button to switch example, refine blur

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,46 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import {
   videoStyle,
   appContainer,
   canvasStyle,
   container,
+  buttonStyle,
+  buttonContainer,
+  heading,
 } from "./styles/app.css";
 
 function App() {
+  const [videoUrl, setVideoUrl] = useState(
+    "https://joy.videvo.net/videvo_files/video/free/2019-01/large_watermarked/181015_13_Venice%20Beach%20Drone_25_preview.mp4"
+  );
+
   const videoRef = useRef<HTMLVideoElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
+  const EXAMPLE_BTN = [
+    {
+      label: "Example 1",
+      url: "https://joy.videvo.net/videvo_files/video/free/2019-01/large_watermarked/181015_13_Venice%20Beach%20Drone_25_preview.mp4",
+    },
+    {
+      label: "Example 2",
+      url: "https://joy.videvo.net/videvo_files/video/free/2016-11/large_watermarked/Smoke_Dark_11_Videvo_preview.mp4",
+    },
+    {
+      label: "Example 3",
+      url: "https://wedistill.io/uploads/videos/processed/1007/2015-03-29-01.mp4.mp4",
+    },
+  ];
+
   return (
     <div className={appContainer}>
+      <h1 className={heading}>AMBIENT VIDEO PLAYER</h1>
       <div className={container}>
         <video
           ref={videoRef}
-          src="https://wedistill.io/uploads/videos/processed/1007/2015-03-29-01.mp4.mp4"
+          src={videoUrl}
           controls
-          width={640}
+          autoPlay
           className={videoStyle}
           onTimeUpdate={() => {
             if (canvasRef.current && videoRef.current) {
@@ -33,12 +56,22 @@ function App() {
             }
           }}
         />
-        <canvas
-          ref={canvasRef}
-          width={640}
-          height={360}
-          className={canvasStyle}
-        />
+        <canvas ref={canvasRef} className={canvasStyle} />
+      </div>
+      <div className={buttonContainer}>
+        {EXAMPLE_BTN.map((data, idx) => {
+          return (
+            <button
+              key={idx}
+              className={buttonStyle}
+              onClick={() => {
+                setVideoUrl(data.url);
+              }}
+            >
+              {data.label.toUpperCase()}
+            </button>
+          );
+        })}
       </div>
     </div>
   );
--- src/styles/app.css.ts
+++ src/styles/app.css.ts
@@ -4,22 +4,51 @@ export const appContainer = style({
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
+  flexDirection: "column",
   minHeight: "100vh",
   minWidth: "100vw",
+  gap: "4rem",
+});
+
+export const heading = style({
+  fontSize: "48px",
+  color: "hsla(0, 0%, 100%, 0.12)",
+  letterSpacing: "-3px",
+  marginTop: "4rem",
 });
 
 export const container = style({
   position: "relative",
+  maxWidth: "1280px",
+  aspectRatio: "16 / 9",
 });
 
 export const videoStyle = style({
   display: "block",
+  width: "100%",
 });
 
 export const canvasStyle = style({
   display: "block",
   position: "absolute",
+  width: "100%",
+  height: "100%",
   top: "0",
-  filter: "blur(40px)",
+  transform: "scale(1.15)",
+  filter: "blur(50px) opacity(0.3)",
   zIndex: -1,
+});
+
+export const buttonContainer = style({
+  display: "flex",
+  gap: "1.5em",
+});
+
+export const buttonStyle = style({
+  all: "unset",
+  padding: "0.5em 1.5em",
+  backgroundColor: "unset",
+  color: "hsl(0, 0%, 35%)",
+  border: "1px solid currentColor",
+  cursor: "pointer",
 });
